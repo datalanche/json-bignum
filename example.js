83628341f8192688c8b2b75aa054a3c8447b0102
rpedela <rpedela@datalanche.com>
2013-03-25 09:59:45 -0600

    Create BigNumber class which just stores a string value for number.

--- example.js
+++ example.js
@@ -1,21 +1,11 @@
 var bignumJSON = require('./lib');
 
-var dec1 = new bignumJSON.Decimal('0.1');
-var dec2 = new bignumJSON.Decimal('0.2');
-var decSum = dec1.add(dec2);
-console.log('0.1 + 0.2 = ' + decSum.toString());
+var testDecimal = '{"normal":-922337203.234,"big":-9223372036854775807.4237482374983253298159}';
+console.log('\ntestDecimal:                   ' + testDecimal);
+console.log('JSON.parse(testDecimal):       ' + JSON.stringify(JSON.parse(testDecimal)));
+console.log('bignumJSON.parse(testDecimal): ' + bignumJSON.stringify(bignumJSON.parse(testDecimal)));
 
-var int1 = new bignumJSON.BigInt('10');
-var int2 = new bignumJSON.BigInt('10');
-var intSum = int1.add(int2);
-console.log('10 + 10 = ' + intSum.toString());
-
-var testDec = '{"normal":-922337203.234,"big":-9223372036854775807.4237482374983253298159}';
-console.log('\ntestDec:                   ' + testDec);
-console.log('JSON.parse(testDec):       ' + JSON.stringify(JSON.parse(testDec)));
-console.log('bignumJSON.parse(testDec): ' + bignumJSON.stringify(bignumJSON.parse(testDec)));
-
-var testInt = '{"normal":-922337203,"big":-92233720368547758074237482374983253298159}';
-console.log('\ntestInt:                   ' + testInt);
-console.log('JSON.parse(testInt):       ' + JSON.stringify(JSON.parse(testInt)));
-console.log('bignumJSON.parse(testInt): ' + bignumJSON.stringify(bignumJSON.parse(testInt)));
+var testInteger = '{"normal":-922337203,"big":-92233720368547758074237482374983253298159}';
+console.log('\ntestInteger:                   ' + testInteger);
+console.log('JSON.parse(testInteger):       ' + JSON.stringify(JSON.parse(testInteger)));
+console.log('bignumJSON.parse(testInteger): ' + bignumJSON.stringify(bignumJSON.parse(testInteger)));
